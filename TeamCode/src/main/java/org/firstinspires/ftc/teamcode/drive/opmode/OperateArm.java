cf4db6e64145c477242891adce60eff1be77ae9d
jrasor <jrasor@boscotech.edu>
2020-07-14 06:39:49 -0700

    Added arm support.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/OperateArm.java
@@ -0,0 +1,115 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * This OpMode scans a single servo back and forwards until Stop is pressed.
+ * The code is structured as a LinearOpMode
+ * INCREMENT sets how much to increase/decrease the servo position each cycle
+ * CYCLE_MS sets the update period.
+ *
+ * This code assumes a Servo configured with the name "arm" as is found on a Trainerbot.
+ *
+ * NOTE: When any servo position is set, ALL attached servos are activated, so ensure that any other
+ * connected servos are able to move freely before running this test.
+ * ToDo: remove that constraint.
+ */
+
+@TeleOp(name = "Operate Arm", group = "Actuators")
+//@Disabled
+public class OperateArm extends LinearOpMode {
+
+    static final double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
+    static final int    CYCLE_MS    =   50;     // period of each cycle
+    static final double STOWED      =  1.0;     // Retracted over robot body
+    static final double DEPLOYED    =  0.0;     // Extended out over Field
+
+    // Define class members
+    Servo   arm;
+    double  position = STOWED;
+
+    @Override
+    public void runOpMode() {
+
+        // Connect to servo (Assume PushBot Left Hand)
+        // Change the text in quotes to match any servo name on your robot.
+        arm = hardwareMap.get(Servo.class, "arm");
+        boolean rampUp = true;
+        // Wait for the start button
+        telemetry.addData(">", "Press Start to activate arm." );
+        telemetry.update();
+        waitForStart();
+
+        // Scan servo till stop pressed.
+        while(opModeIsActive()){
+
+            if (gamepad1.x) {
+                rampUp = true;
+            }
+            if (gamepad1.y) {
+                rampUp = false;
+            }
+
+            //   Slowly pull the arm into the retracted position
+            if (rampUp) {
+                // Keep stepping up until we hit the retracted value.
+                position += INCREMENT ;
+                if (position >= STOWED ) {
+                    position = STOWED;
+                }
+            }
+            else {
+                //   Slowly extend arm out over the Field
+                position -= INCREMENT ;
+                if (position <= DEPLOYED ) {
+                    position = DEPLOYED;
+                }
+            }
+
+            // Display the current value
+            telemetry.addData("Arm Position", "%5.2f", position);
+            telemetry.addData(">", "Press Stop to end test." );
+            telemetry.update();
+
+            // Set the servo to the new position and pause;
+            arm.setPosition(position);
+            sleep(CYCLE_MS);
+            idle();
+        }
+
+        // Signal done;
+        telemetry.addData(">", "Done");
+        telemetry.update();
+    }
+}
